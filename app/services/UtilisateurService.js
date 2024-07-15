9fe3a8474d7aefc6c8982ac090b557ef4fe3be76
Mahm <marsonambinintsoa@gmail.com>
2024-07-15 16:20:27 +0300

    mis à jour ndray

--- app/services/UtilisateurService.js
+++ app/services/UtilisateurService.js
@@ -1,5 +1,6 @@
 const Utilisateur = require('../models/Utilisateur');
 const MessageService = require('./MessageService');
+const mongoose=require('mongoose');
 
 class UtilisateurService {
   // Créer un nouvel utilisateur
@@ -77,42 +78,78 @@ class UtilisateurService {
       throw error;
     }
   }
-  async getAllUtilisateur(sessionUserId) {
+
+  async  getAllUtilisateur(sessionUserId) {
     try {
-      const utilisateurs = await Utilisateur.find({ _id: { $ne: sessionUserId } }).populate('groupes stories');
-      const users = [];
+      const Utilisateur = mongoose.model('Utilisateur');
+      const Groupe = mongoose.model('Groupe');
+  
+      const contacts = [];
+  
+      // Récupérer tous les utilisateurs sauf l'utilisateur en session
+      const utilisateurs = await Utilisateur.find({ _id: { $ne: sessionUserId } }).populate('stories');
   
       utilisateurs.forEach(utilisateur => {
-        const groups = [];
-        utilisateur.groupes.forEach(groupe => {
-          const group = {
-            _id: groupe._id,
-            nom: groupe.nom,
-            description: groupe.description,
-            photo: groupe.photo,
-            createur: groupe.createur
-          };
-          groups.push(group);
-        });
-  
-        const user = {
-          _id: utilisateur._id,
+        // Structurer les utilisateurs en tant que contacts
+        const userContact = {
+          _id: utilisateur._id.toString(),
+          type: 'utilisateur',
           nom: utilisateur.nom,
           presence: utilisateur.presence,
-          email: utilisateur.email,
           photo: utilisateur.photo,
-          stories: utilisateur.stories,
-          groupes: groups
+          story: utilisateur.stories.length // Longueur des stories
         };
-        users.push(user);
+        contacts.push(userContact);
       });
   
-      return users;
+      // Récupérer les groupes auxquels l'utilisateur en session appartient
+      const groupes = await Groupe.find({ membres: sessionUserId });
+  
+      groupes.forEach(groupe => {
+        // Structurer les groupes en tant que contacts (sans présence)
+        const groupContact = {
+          _id: groupe._id.toString(),
+          type: 'groupe',
+          nom: groupe.nom,
+          presence: '', // Pas de présence pour les groupes
+          photo: groupe.photo
+        };
+        contacts.push(groupContact);
+      });
+  
+      return contacts;
     } catch (error) {
-      console.error('Erreur lors de la récupération des utilisateurs :', error);
+      console.error('Erreur lors de la récupération des utilisateurs et des groupes :', error);
       throw error;
     }
   }
+  async  getAllUser() {
+    try {
+      const Utilisateur = mongoose.model('Utilisateur');
+      const contacts = [];
+      // Récupérer tous les utilisateurs sauf l'utilisateur en session
+      const utilisateurs = await Utilisateur.find().populate('stories');
+  
+      utilisateurs.forEach(utilisateur => {
+        // Structurer les utilisateurs en tant que contacts
+        const userContact = {
+          _id: utilisateur._id.toString(),
+          type: 'utilisateur',
+          nom: utilisateur.nom,
+          presence: utilisateur.presence,
+          photo: utilisateur.photo,
+          story: utilisateur.stories.length // Longueur des stories
+        };
+        contacts.push(userContact);
+      });
+  
+      return contacts;
+    } catch (error) {
+      console.error('Erreur lors de la récupération des utilisateurs et des groupes :', error);
+      throw error;
+    }
+  }
+
 
 
   // Mettre à jour un utilisateur
@@ -465,52 +502,54 @@ class UtilisateurService {
       throw error;
     }
   }
-  async searchUtilisateurs(parametre, sessionUserId) {
+  async  searchUtilisateurs(parametre, sessionUserId) {
     try {
       const regex = new RegExp(parametre, 'i'); // 'i' pour une recherche insensible à la casse
+      const Utilisateur = mongoose.model('Utilisateur');
+      const Groupe = mongoose.model('Groupe');
+  
+      // Rechercher les utilisateurs correspondant au paramètre
       const utilisateurs = await Utilisateur.find({
         _id: { $ne: sessionUserId }, // Exclure l'utilisateur en session
-        $or: [
-          { nom: { $regex: regex } },
-          { email: { $regex: regex } }
-        ]
-      }).populate('groupes stories'); // Ajout de 'stories' à la population si nécessaire
+        nom: { $regex: regex }
+      }).populate('stories'); // Ajouter 'stories' à la population
   
-      if (!utilisateurs) {
-        const error = new Error('Utilisateurs non trouvés.');
-        error.status = 404;
-        throw error;
-      }
-  
-      const users = [];
-      utilisateurs.forEach(utilisateur => {
-        const groups = [];
-        utilisateur.groupes.forEach(groupe => {
-          const group = {
-            _id: groupe._id,
-            nom: groupe.nom,
-            description: groupe.description,
-            photo: groupe.photo,
-            createur: groupe.createur
-          };
-          groups.push(group);
-        });
-  
-        const user = {
-          _id: utilisateur._id,
-          nom: utilisateur.nom,
-          presence: utilisateur.presence,
-          email: utilisateur.email,
-          photo: utilisateur.photo,
-          stories: utilisateur.stories,
-          groupes: groups
-        };
-        users.push(user);
+      // Rechercher les groupes auxquels l'utilisateur en session appartient et correspondant au paramètre
+      const groupes = await Groupe.find({
+        membres: sessionUserId,
+        nom: { $regex: regex }
       });
   
-      return users;
+      const contacts = [];
+  
+      utilisateurs.forEach(utilisateur => {
+        // Structurer les utilisateurs en tant que contacts
+        const userContact = {
+          _id: utilisateur._id.toString(),
+          type: 'utilisateur',
+          nom: utilisateur.nom,
+          presence: utilisateur.presence,
+          photo: utilisateur.photo,
+          story: utilisateur.stories.length // Longueur des stories
+        };
+        contacts.push(userContact);
+      });
+  
+      groupes.forEach(groupe => {
+        // Structurer les groupes en tant que contacts (sans présence)
+        const groupContact = {
+          _id: groupe._id.toString(),
+          type: 'groupe',
+          nom: groupe.nom,
+          presence: '', // Pas de présence pour les groupes
+          photo: groupe.photo
+        };
+        contacts.push(groupContact);
+      });
+  
+      return contacts;
     } catch (error) {
-      console.error('Erreur lors de la recherche des utilisateurs :', error);
+      console.error('Erreur lors de la recherche des utilisateurs et des groupes :', error);
       throw error;
     }
   }
