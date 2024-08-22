66fa0c60a271b3688cee0db2a2edcecfe8ce4830
ambinintsoamarckel <142052366+ambinintsoamarckel@users.noreply.github.com>
2024-08-22 06:22:54 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const loadFixture= require('../app/fixtures/Fixtures')
-//const dbUrl = 'mongodb+srv://mahm:mahm@cluster0.qaxiuic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';// Remplacez par votre URL de connexion
-const dbUrl = 'mongodb://mahm:mahm@127.0.0.1:27017/Tempest?retryWrites=true&w=majority&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6';
+const dbUrl = 'mongodb+srv://mahm:mahm@cluster0.qaxiuic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';// Remplacez par votre URL de connexion
+//const dbUrl = 'mongodb://mahm:mahm@127.0.0.1:27017/Tempest?retryWrites=true&w=majority&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6';
 const MongoStore = require('connect-mongo');
 mongoose.connect(dbUrl, {
   dbName: 'Tempest',
